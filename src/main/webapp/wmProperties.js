dbad04310f67494807dd2564f5c5cdbedff0a297
root <root@bd22575766a5>
2017-04-07 05:18:17 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "testnewproj_mvnbuild",
+  "homePage" : "Main",
+  "name" : "testnewproj_mvnbuild",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
